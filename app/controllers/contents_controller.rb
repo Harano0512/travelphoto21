95088a87b2afc187e14c4fe810b4b84232e12afe
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-05-31 19:31:02 +0900

    間違えて削除したところを復活

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,7 +2,7 @@ class ContentsController < ApplicationController
   before_action :authenticate_user!, expect: [:index]
 
   def index
-    
+   @contents = Content.order('created_at DESC')
   end
 
   def new
@@ -13,13 +13,36 @@ class ContentsController < ApplicationController
     @content = Content.new(content_params)
     if @content.save
       redirect_to new_content_path(@content.id)
+      redirect_to contents_path
     else
       render :new
     end
   end
 
+  def show
+    @content = Content.find(params[:id])
+  end
+
+  def destroy
+    @content = Content.find(params[:id])
+    @content.destroy
+    redirect_to root_path
+  end
+
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  def update
+    @content = Content.find(params[:id])
+    if @content.update(content_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
-  
   def content_params
     params.require(:content).permit(:title, :from_date, :return_date).merge(user_id: current_user.id)
   end

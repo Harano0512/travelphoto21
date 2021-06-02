a1f6ffabda81269bf7af482bd066a2128d6857d7
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-06-02 16:19:49 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,19 @@ class PostsController < ApplicationController
     redirect_to content_path(@post.content.id)
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to content_post_path(@post.content_id,@post.id)
+    else
+      render :edit
+    end
+  end
+
 
   private
   def post_params

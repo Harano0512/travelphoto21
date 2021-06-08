cd4b50ea6e89d16d785683f8dd7fb3a9fd264b8c
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-06-09 00:06:21 +0900

    表示順序の変更

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,6 +1,6 @@
 class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:id])
-    @posts = Post.where(prefecture_id: params[:id]).page(params[:page]).per(10)
+    @posts = Post.where(prefecture_id: params[:id]).order("travel_date ASC").page(params[:page]).per(10)
   end
 end

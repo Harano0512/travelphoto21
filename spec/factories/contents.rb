6d8cac33ce61cc6399ddbf56e0f3d27d17f149bc
Harano0512 <78680327+Harano0512@users.noreply.github.com>
2021-06-11 14:03:04 +0900

    postモデル単体テスト

--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :content do
-    title             {Faker::String.random(length: 8)}
+    title             {Faker::Lorem.word}
     from_date         {Faker::Date.between(from: '2021-05-01', to: '2021-05-03')}
     return_date       {Faker::Date.between(from: '2021-05-04', to: '2021-05-06')}
   end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :post do
+  describe 'content新規登録' do
+    before do
+      user = FactoryBot.create(:user)
+      content = FactoryBot.create(:content, user_id: user.id)
+      @post = FactoryBot.build(:post,content_id: content.id,user_id: user.id)
+    end
+  
+    context '内容に問題がない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@post).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'タイトルが空では保存ができない' do
+        @post.subject=""
+        @post.valid?
+        expect(@post.errors.full_messages).to include('タイトルを入力してください')
+      end
+      it '日付が空では保存ができない' do
+        @post.travel_date=""
+        @post.valid?
+        expect(@post.errors.full_messages).to include('日付を入力してください')
+      end
+      it '都道府県が空では保存ができない' do
+        @post.prefecture_id=""
+        @post.valid?
+        expect(@post.errors.full_messages).to include('都道府県を選択してください')
+      end
+      it '都道府県を選択しないと保存ができない' do
+        @post.prefecture_id=1
+        @post.valid?
+        expect(@post.errors.full_messages).to include('都道府県を選択してください')
+      end
+      it 'テキストが空では保存ができない' do
+        @post.article=""
+        @post.valid?
+        expect(@post.errors.full_messages).to include('テキストを入力してください')
+      end
+      it '写真を選択しないと保存ができない' do
+        @post.images=nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('写真を入力してください')
+      end
+    end
+  
+  end
+end
